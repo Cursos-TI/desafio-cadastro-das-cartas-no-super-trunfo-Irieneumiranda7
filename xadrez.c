52c6f7d1f6691e2f0a0bc353f778ec1f4d7e0b97
Irineu Miranda <irineumiranda95@gmail.com>
2025-09-04 00:01:13 +0000

    desafio aventureiro realizado

--- xadrez.c
+++ xadrez.c
@@ -3,39 +3,44 @@
 int main(){
 
  char estado1[20], estado2[20], codigo1[10], codigo2[10], cidade1[20], cidade2[20];
- int PontosTuristicos;
- float km², populacao, PIB;
+ int PontosTuristicos1, PontosTuristicos2;
+ float km²_1, populacao1, PIB1, DensidadePopulacional1, PIBperCapita1; 
+ float km²_2, populacao2, PIB2, DensidadePopulacional2, PIBperCapita2; 
  
   printf("\n--- preencha os dados das cartas usando somente pontos no lugar de virgulas---\n");
 
 
  printf("digite o estado 1: \n");
- scanf(" %s", estado1);
+ scanf("%s", estado1);
 
  printf("digite a cidade 1: \n");
- scanf(" %s", cidade1);
+ scanf("%s", cidade1);
 
  printf("digite um codigo da carta 1: \n");
- scanf(" %s", codigo1);
+ scanf("%s", codigo1);
 
  printf("digite a populacao da carta 1: \n");
- scanf(" %f", &populacao);
+ scanf("%f", &populacao1);
 
  printf("digite os pontos turisticos da carta 1: \n");
- scanf(" %d", &PontosTuristicos);
+ scanf("%d", &PontosTuristicos1);
 
  printf("digite o PIB da carta 1: \n");
- scanf(" %f", &PIB);
+ scanf("%f", &PIB1);
 
  printf("digite a km² da carta 1: \n");
- scanf(" %f", &km²);
+ scanf("%f", &km²_1);
+
+ DensidadePopulacional1 = populacao1 / km²_1;
+ PIBperCapita1 = PIB1 / populacao1;
 
  printf("\n--- Dados da Carta 01---\n");
 
  printf("estado: %s - cidade: %s - codigo: %s\n", estado1, cidade1, codigo1);
- printf("populacao: %.2f - km²: %.2f - pontos turisticos: %d\n", populacao, km², PontosTuristicos);
- printf("PIB: %.2f\n", PIB);
-
+ printf("populacao: %.2f - km²: %.2f - pontos turisticos: %d\n", populacao1, km²_1, PontosTuristicos1);
+ printf("PIB: %.2f\n", PIB1);
+ printf("A densidade populacional é: %.2f\n", DensidadePopulacional1);
+ printf("O PIBperCapita é: %f.2\n", PIBperCapita1);
 
 
 
@@ -54,25 +59,28 @@ int main(){
  scanf(" %s", codigo2);
 
  printf("digite a populacao da carta 2: \n");
- scanf(" %f", &populacao);
+ scanf(" %f", &populacao2);
+
 
  printf("digite os pontos turisticos da carta 2: \n");
- scanf(" %d", &PontosTuristicos);
+ scanf(" %d", &PontosTuristicos2);
 
  printf("digite o PIB da carta 2: \n");
- scanf(" %f", &PIB);
+ scanf(" %f", &PIB2);
 
  printf("digite a km² da carta 2: \n");
- scanf(" %f", &km²);
+ scanf(" %f", &km²_2);
 
-
+ DensidadePopulacional2 = populacao2 / km²_2;
+ PIBperCapita1 = PIB2 / populacao2;
 
  printf("\n--- Dados da Carta 02---\n");
 
  printf("estado: %s - cidade: %s - codigo: %s\n", estado2, cidade2, codigo2);
- printf("populacao: %.2f - km²: %.2f - pontos turisticos: %d\n", populacao, km², PontosTuristicos);
- printf("PIB: %.2f\n", PIB);
-
+ printf("populacao: %.2f - km²: %.2f - pontos turisticos: %d\n", populacao2, km²_2, PontosTuristicos2);
+ printf("PIB: %.2f\n", PIB2);
+ printf("A densidade populacional é: %.2f\n", DensidadePopulacional2);
+ printf("O PIBperCapita é: %.2f\n", PIBperCapita2);
  return 0;
 
 
